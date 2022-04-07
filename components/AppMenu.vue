b68d67de4a49ea5df429df6c869cd0730b679219
toyosi <toyosiazuh.at@gmail.com>
2022-04-07 23:34:53 +0100

    initial commit

---
+++ components/AppMenu.vue
@@ -0,0 +1,8 @@
+<template>
+  <nav>
+    <ul>
+      <li><nuxt-link exact to="/">Home</nuxt-link></li>
+      <li><nuxt-link to="/restaurants">Restaurant</nuxt-link></li>
+    </ul>
+  </nav>
+</template>
---
+++ layouts/default.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <div class="content">
+      <AppMenu />
+      <Nuxt />
+      <AppFooter/>
+    </div>
+  </div>
+</template>
+
+<script>
+import AppMenu from '@/components/AppMenu.vue'
+import AppFooter from '@/components/AppFooter.vue'
+
+export default {
+  components: {
+    AppMenu,
+    AppFooter
+  }
+}
+</script>
+
+<style scoped>
+html {
+  font-family: "Mulish";
+  font-weight: 16px;
+  font-weight: 300;
+}
+
+h1,
+h2,
+h3,
+h4 {
+  font-family: "Poppins";
+  font-weight: 16px;
+  font-weight: 600;
+}
+</style>
